f59cbceea40e3e2396f0e752e0411857844806ea
P202U <156369604+P202U@users.noreply.github.com.>
2024-12-17 15:51:59 +0530

    Check deploy

--- config.py
+++ config.py
@@ -4,9 +4,15 @@ from app import app
 
 load_dotenv()
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv(
-    "SQLALCHEMY_TRACK_MODIFICATIONS"
-)
+# Use environment variables to set database URI
+if os.environ.get('DATABASE_URL'):
+    # This will be used for Vercel (production)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    # This will be used for local development
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+# Additional configurations
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["IMAGES"] = os.getenv("IMAGES")
