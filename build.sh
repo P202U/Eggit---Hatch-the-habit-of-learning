34ce66b7a795b6103457e73a4a6554c2a2847614
P202U <156369604+P202U@users.noreply.github.com.>
2024-12-17 18:57:38 +0530

    Try render

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 pip install --upgrade pip
 pip install psycopg2-binary==2.9.3
 pip install -r requirements.txt
+# postgresql://eggit_postgresql_user:Evk5bVBYHmTUR5dmQHYbMeZU76xy0fLv@dpg-ctgl53qj1k6c73a2vbtg-a.oregon-postgres.render.com/eggit_postgresql
